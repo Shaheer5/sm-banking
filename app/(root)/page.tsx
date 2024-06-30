f5de03a8ff5f23d84bd4ca65ae07a2d1fa35df51
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-06-30 14:22:05 +0500

    "Added HeaderBox component to Home page and updated layout"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = { firstName: "Shaheer" };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access & manage your accounts and transactions efficiently."
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
