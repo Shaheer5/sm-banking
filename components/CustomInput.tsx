543e769798997ca20bca1c9d98bfe9cd5edf8765
Shaheer Mansoor <shaheermansoor321@gmail.com>
2024-07-12 23:15:53 +0500

    implement authForm half way

---
+++ components/CustomInput.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import {
+  FormControl,
+  FormField,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { z } from "zod";
+import { authFormSchema } from "@/lib/utils";
+
+interface CustomInputProps {
+  name: FieldPath<z.infer<typeof authFormSchema>>;
+  control: Control<z.infer<typeof authFormSchema>>;
+  label: string;
+  placeholder: string;
+}
+
+// Main component
+const CustomInput = ({
+  name,
+  control,
+  label,
+  placeholder,
+}: CustomInputProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex flex-col w-full">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                type={name === "password" ? 'password' : 'text'}
+                {...field}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
